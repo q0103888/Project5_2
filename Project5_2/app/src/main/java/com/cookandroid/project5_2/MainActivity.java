b5ae7ab31225160e72adc37ada03fcb8243426b0
q0103888 <72806455+q0103888@users.noreply.github.com>
2021-10-12 19:01:41 +0900

    10/12 android class

--- Project5_2/app/src/main/java/com/cookandroid/project5_2/MainActivity.java
+++ Project5_2/app/src/main/java/com/cookandroid/project5_2/MainActivity.java
@@ -3,6 +3,7 @@ package com.cookandroid.project5_2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.graphics.Color;
 import android.media.Image;
 import android.os.Bundle;
@@ -14,97 +15,40 @@ import android.widget.Chronometer;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.ViewFlipper;
 
 import org.w3c.dom.Text;
 
 import java.util.Date;
-
-public class MainActivity extends AppCompatActivity {
-    Chronometer chrono;
-    RadioButton rdoCal, rdoTime;
-    DatePicker dpickerView;
-    TimePicker tPicker;
-    TextView tvYear, tvMonth, tvDay, tvHour, tvMin;
-    int selectYear, selectMonth, selectDay;
-
+//기존은 AppCompatAcivity
+//
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeset);
-        setTitle("시간 예약");
+        setContentView(R.layout.activity_taphost);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpecSong = tabHost.newTabSpec("Song").setIndicator("음악별");
+        tabSpecSong.setContent(R.id.tabsong);
+        tabHost.addTab(tabSpecSong);
+
+        TabHost.TabSpec tabSpecArtist = tabHost.newTabSpec("Artist").setIndicator("가수별");
+        tabSpecArtist.setContent(R.id.tabArtist);
+        tabHost.addTab(tabSpecArtist);
+
+        TabHost.TabSpec tabSpecAlbum = tabHost.newTabSpec("Album").setIndicator("앨범별");
+        tabSpecAlbum.setContent(R.id.tabAlbum);
+        tabHost.addTab(tabSpecAlbum);
+
+        tabHost.setCurrentTab(0);
 
 
-        chrono = (Chronometer)findViewById(R.id.chronometer1);
-
-        rdoCal = (RadioButton)findViewById(R.id.rdoCal);
-        rdoTime = (RadioButton)findViewById(R.id.rdoTime);
-
-        tPicker = (TimePicker)findViewById(R.id.timePicker1);
-        dpickerView = (DatePicker)findViewById(R.id.dpickerView1);
-
-        tvYear = (TextView)findViewById(R.id.tvYear);
-        tvMonth = (TextView)findViewById(R.id.tvMonth);
-        tvDay = (TextView)findViewById(R.id.tvDay);
-        tvHour = (TextView)findViewById(R.id.tvHour);
-        tvMin = (TextView)findViewById(R.id.tvMin);
-
-        rdoCal.setVisibility(View.INVISIBLE);
-        rdoTime.setVisibility(View.INVISIBLE);
-        tPicker.setVisibility(View.INVISIBLE);
-        dpickerView.setVisibility(View.INVISIBLE);
-
-        rdoCal.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tPicker.setVisibility(View.INVISIBLE);
-                dpickerView.setVisibility(View.VISIBLE);
-            }
-        });
-
-        rdoTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tPicker.setVisibility(View.VISIBLE);
-                dpickerView.setVisibility(View.INVISIBLE);
-            }
-        });
-
-
-        chrono.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                chrono.setBase(SystemClock.elapsedRealtime());
-                chrono.start();
-                chrono.setTextColor(Color.RED);
-                rdoCal.setVisibility(View.VISIBLE);
-                rdoTime.setVisibility(View.VISIBLE);
-            }
-        });
-
-
-
-        tvYear.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                chrono.stop();
-                chrono.setTextColor(Color.BLUE);
-                tvYear.setText(Integer.toString(dpickerView.getYear()));
-                tvMonth.setText(Integer.toString(dpickerView.getMonth()+1));
-                tvDay.setText(Integer.toString(dpickerView.getDayOfMonth()));
-
-                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
-                tvMin.setText(Integer.toString(tPicker.getCurrentMinute()));
-
-                rdoCal.setVisibility(View.INVISIBLE);
-                rdoTime.setVisibility(View.INVISIBLE);
-                tPicker.setVisibility(View.INVISIBLE);
-                dpickerView.setVisibility(View.INVISIBLE);
-                return false;
-            }
-        });
 
 
 
